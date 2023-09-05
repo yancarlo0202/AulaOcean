70d5a3acc49236f5d534b28e73ab0d38b50a8964
Yan Carlo Vieira Braga <yancarloaluno@gmail.com>
2023-09-05 00:54:58 +0000

    sei lá

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask 
+from flask import Flask, render_template
+
+#app = Flask("meu app")
+
+#@app.route('/')
+#def hello():
+#    return "Hello Yan!"
+
+post = [
+    {
+        "título": "Minha primeira postagem",
+        "texto": "teste"
+    }
+
+    {
+        "título": "Minha segunda postagem",
+        "texto": "teste"
+    }
+]
 
 app = Flask("meu app")
 
 @app.route('/')
-def hello():
-    return "Hello world!"
+def exibir_entradas():
+    entradas = post
+    return render_template('exibir_entrada.hmtl')
 
-@app.route('/novo')
-def novo():
-    return "<h1>Nova página</h1>"
+
+#@app.route('/novo')
+#def novo():
+#    return "<h1>Nova página</h1>"
